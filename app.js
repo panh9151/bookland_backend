d91a610066d23124e94d9c8907741e27c0ecdec5
TienPhatPAT <138203349+TienPhatPAT@users.noreply.github.com>
2024-07-19 21:08:32 +0700

    ád

--- app.js
+++ app.js
@@ -32,12 +32,9 @@ const mongoose = require("mongoose");
 //Connect database
 mongoose
   .connect(
-    process.env.DB_URL,
+    process.env.DB_URL
     // "mongodb+srv://panh9151:furnito123@furnito.8i9doe9.mongodb.net/furnito",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
+
   )
   .then(() => console.log(">>>>>>>>>> DB Connected!!!!!!"))
   .catch((err) => console.log(">>>>>>>>> DB Error: ", err));
