c302725284124a4d36a7706caccd19683bc4e53c
TienPhatPAT <138203349+TienPhatPAT@users.noreply.github.com>
2024-07-31 21:59:35 +0700

    bugs
    còn clodinari

--- models/DonHang/DonHangModel.js
+++ models/DonHang/DonHangModel.js
@@ -2,21 +2,57 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const DonHangSchema = new Schema({
-  id_nguoidung: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const DonHangSchema = new Schema(
+  {
+    id_nguoidung: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    diachi: {
+      type: String,
+      required: true,
+    },
+    sdt: {
+      type: String,
+      required: true,
+    },
+    nguoinhan: {
+      type: String,
+    },
+    phuongthucthanhtoan: {
+      type: Number,
+      required: true,
+      enum: [0, 1], // 0: tiền mặt, 1: ngân hàng
+    },
+    ghichu: {
+      type: String,
+    },
+    ngaydathang: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: Number,
+      required: true,
+      enum: [0, 1, 2, 3, 4], // 0: chờ duyệt, 1: đang giao hàng, 2: đã giao hàng, 3: hủy, 4: trả hàng
+    },
+    thanhtoan: {
+      type: Number,
+      required: true,
+      enum: [0, 1], // 0: chưa thanh toán, 1: đã thanh toán
+    },
+    chitietdonhangs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "ChiTietDonHang",
+      },
+    ], // Mảng chứa ID các chi tiết đơn hàng
   },
-  diachi: { type: String, required: true },
-  sdt: { type: Number, required: true },
-  nguoinhan: { type: String },
-  phuongthucthanhtoan: { type: Number, required: true, enum: [0, 1] }, // 0: tienmat, 1: nganhang
-  ghichu: { type: String },
-  ngaydathang: { type: Date, required: true },
-  status: { type: Number, required: true, enum: [0, 1, 2, 3, 4] }, // 0: choduyet, 1: danggiaohang, 2: dagiaohang, 3: huy, 4: trahang
-  thanhtoan: { type: Number, required: true, enum: [0, 1] }, // 0: chuathanhtoan, 1: dathanhtoan
-});
+  {
+    timestamps: true, // Tự động thêm các trường createdAt và updatedAt
+  }
+);
 
 const DonHangModel = mongoose.model("DonHang", DonHangSchema);
 
