9e5a43d12fb9d21073c61dc1161e98e955ce7b7c
TienPhatPAT <138203349+TienPhatPAT@users.noreply.github.com>
2024-08-08 22:26:39 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 const cors = require("cors");
-const bodyParser = require("body-parser");
-const express = require("express");
-const path = require("path");
-// const multer = require("multer");
-const router = express.Router();
-const logger = require("morgan");
-const createError = require("http-errors");
-const cookieParser = require("cookie-parser");
+const routerAdmin = require("./routes/Admin.js");
+const routerNguoiDung = require("./routes/NguoiDung.js");
+const routerSach = require("./routes/Sach.js");
+const routerSachDetail = require("./routes/SachDetail.js");
+const routerTacgia = require("./routes/Tacgia.js");
+const routerBanner = require("./routes/Banner.js");
+const routerDanhGia = require("./routes/DanhGia.js");
+const routerDonHang = require("./routes/DonHang.js");
+const routerTheLoaiSach = require("./routes/TheLoaiSach.js");
+const routerTheLoaiDetail = require("./routes/TheLoaiDetail.js");
+const routerAuth = require("./routes/Auth.js");
+const routerBaiViet = require("./routes/BaiViet.js");
+const routerBinhLuan = require("./routes/BinhLuan.js");
+const routerChiTietDonHang = require("./routes/ChiTietDonHang.js");
+const routerSachYeuThich = require("./routes/SachYeuThich.js");
+const routerTimKiem = require("./routes/TimKiem.js");
+const routerLuotTim = require("./routes/LuotTim.js");
+var express = require("express");
+var path = require("path");
+// var cors = require("cors");
+var router = express.Router();
+var logger = require("morgan");
+var createError = require("http-errors");
+var cookieParser = require("cookie-parser");
+
+// Library declaration
+const mongoose = require("mongoose");
+
+// require("./models/UserModel");
+// require("./models/CategoryModel");
+
+//Connect database
+mongoose
+  .connect(process.env.DB_URL) // Link đến DB_URL trong file .env
+  // .connect("mongodb://localhost:27017/Bookland")
+  .then(() => console.log(">>>>>>>>>> DB Connected!!!!!!"))
+  .catch((err) => console.log(">>>>>>>>> DB Error: ", err));
+
+// var clientRouter = require("./routes/api");
 
 var app = express();
 const port = process.env.PORT;
@@ -25,37 +56,31 @@ app.get("/", (req, res) => {
   res.send("API Working");
 });
 
-// app.use(logger("dev"));
-// // app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
-const UserRouter = require("./routes/UserAPI");
-const AuthRouter = require("./routes/AuthenAPI");
-const AuthorRouter = require("./routes/AuthorAPI");
-const BannerRouter = require("./routes/BannerAPI");
-const BookRouter = require("./routes/BookAPI");
-const CommentRouter = require("./routes/CommentAPI");
-const RatingRouter = require("./routes/RatingAPI");
-const OrderRouter = require("./routes/OrderAPI");
-const SearchRouter = require("./routes/SearchAPI");
-const BookTypeRouter = require("./routes/BookTypeAPI");
+app.use(logger("dev"));
+// app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// express.urlencoded({ extended: true });
-
-app.use("/nguoidung", UserRouter);
-app.use("/tacgia", AuthorRouter);
-app.use("/banner", BannerRouter);
-app.use("/auth", AuthRouter);
-app.use("/sach", BookRouter);
-app.use("/binhluan", CommentRouter);
-app.use("/danhgia", RatingRouter);
-app.use("/donhang", OrderRouter);
-app.use("/theloai", BookTypeRouter);
-app.use("/search", SearchRouter);
+// app.use("/api/add, routerSach");
+app.use("/api/admin", routerAdmin);
+app.use("/api/nguoidung", routerNguoiDung);
+app.use("/api/sach", routerSach);
+app.use("/api/chitietsach", routerSachDetail);
+app.use("/api/tacgia", routerTacgia);
+app.use("/api/banner", routerBanner);
+app.use("/api/danhgia", routerDanhGia);
+app.use("/api/donhang", routerDonHang);
+app.use("/api/theloai", routerTheLoaiSach);
+app.use("/api/chitiettheloai", routerTheLoaiDetail);
+app.use("/api/auth", routerAuth);
+app.use("/api/baiviet", routerBaiViet);
+app.use("/api/binhluan", routerBinhLuan);
+app.use("/api/chitietdonhang", routerChiTietDonHang);
+app.use("/api/sachyeuthich", routerSachYeuThich);
+app.use("/api/timkiem", routerTimKiem);
+app.use("/api/luottim", routerLuotTim);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
